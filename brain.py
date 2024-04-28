c447c360c2969db32e291340b7262f052669963b
adityasawaks <114384061+adityasawaks@users.noreply.github.com>
2024-04-28 22:14:06 +0530

    Update brain.py

--- brain.py
+++ brain.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
 def preprocess_mri(image_path):
     img = load_img(image_path, target_size=(255, 255))
