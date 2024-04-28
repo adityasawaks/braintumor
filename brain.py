74faaa7403259e864b238504085bd9026fa2b9f7
adityasawaks <114384061+adityasawaks@users.noreply.github.com>
2024-04-28 20:34:06 +0530

    Update brain.py

--- brain.py
+++ brain.py
@@ -7,21 +7,6 @@ st.title("Brain Tumor MRI Classification")
 # Define the class labels
 class_labels = ['glioma', 'meningioma', 'notumor', 'pituitary']
 
-# Function to load the model
-def load_model():
-    try:
-        model = tf.keras.models.load_model("cnn_model.h5")
-    except OSError:
-        st.error("Error: Unable to load the model.")
-        return None
-
-# Function to preprocess the image
-def preprocess_image(image):
-    img = image.resize((255, 255))
-    img_array = tf.keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)
-    return img_array
-
 # File uploader
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
 
@@ -31,11 +16,15 @@ if uploaded_file is not None:
     st.image(image, caption='Uploaded MRI Image', use_column_width=True)
 
     # Load the model
-    model = load_model()
-
-    if model is not None:
+    try:
+        model = tf.keras.models.load_model("cnn_model.h5")
+    except OSError:
+        st.error("Error: Unable to load the model.")
+    else:
         # Preprocess the image
-        img_array = preprocess_image(image)
+        img = image.resize((255, 255))
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)
 
         # Make predictions
         predictions = model.predict(img_array)
